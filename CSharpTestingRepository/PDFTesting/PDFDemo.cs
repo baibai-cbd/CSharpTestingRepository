6989580f65a682cc5c0eb98dbbae4353b82e6640
Yuhao Dong <ydong119@gmail.com>
2020-02-27 23:27:42 -0500

    installed PDFSharp-MigraDoc

---
+++ CSharpTestingRepository/PDFTesting/PDFDemo.cs
@@ -0,0 +1,17 @@
+﻿using MigraDoc.DocumentObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSafeRepository.PDFTesting
+{
+    public class PDFDemo
+    {
+        public static void PDFTestingMethod()
+        {
+            var document = new Document();
+        }
+    }
+}
